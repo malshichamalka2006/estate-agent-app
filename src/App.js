6360a78a89a06e862890c3bab077e8f6653a8f74
malshichamalka48-ui <malshichamalka48@gmail.com>
2026-01-02 22:01:31 +0530

    delelted unwanted file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SearchPage from './pages/SearchPage';
+import PropertyPage from './pages/PropertyPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<SearchPage />} />
+          <Route path="/property/:id" element={<PropertyPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/PropertyPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PropertyPage() {
+  return <h1>Property Details - Coming Soon</h1>;
+}
+
+export default PropertyPage;
